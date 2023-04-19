13b36306ea33d2b7d7f8d76a4d94410872fae3f4
Sebastian Sala <sebastian_orko@hotmail.com>
2023-04-19 13:49:14 -0600

    buildTree funcion added

---
+++ index.js
@@ -0,0 +1,74 @@
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class Tree {
+    constructor(arrayTree = []) {
+      this.root = this.buildTree(arrayTree);
+    }
+  
+    buildTree(arrayTree) {
+      if (!arrayTree.length) {
+        return null;
+      }
+      let sortedArray = mergeSort(arrayTree);
+      let constructedTree = constructTree(sortedArray);
+      return constructedTree;
+    }
+  }
+  
+  
+  function constructTree(arrayTree){
+      if(!arrayTree.length){
+          return null;
+      }
+      let mid = Math.floor(arrayTree.length/2);
+      let left = arrayTree.slice(0, mid);
+      let right = arrayTree.slice(mid + 1);
+      let node = new Node(arrayTree[mid]);
+      node.left = constructTree(left);
+      node.right = constructTree(right);
+      return node;
+  }
+  
+  function mergeSort(arrayTree) {
+    if (arrayTree.length <= 1) {
+      return arrayTree;
+    }
+    let mid = Math.floor(arrayTree.length / 2);
+    let left = arrayTree.slice(0, mid);
+    let right = arrayTree.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    const arrayMerged = [];
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        arrayMerged.push(left.shift());
+      } else {
+        arrayMerged.push(right.shift());
+      }
+    }
+    return arrayMerged.concat(left).concat(right);
+  }
+  
+  function prettyPrint(node, prefix = '', isLeft = true){
+    if (node === null) {
+       return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+  }
+  
+  const myTree = new Tree([2, 3, 1, 5, 10, 7]);
+  prettyPrint(myTree.root);
